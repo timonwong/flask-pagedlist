79f8734a4736093fbed10ce554f45c4031dadbac
Timon Wong <timon86.wang@gmail.com>
2014-06-19 16:00:54 +0800

    Add pagedlist_static_for, and winf.unobtrusive-ajax is now bundled

--- flask_pagedlist/__init__.py
+++ flask_pagedlist/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 
 import jinja2
+from flask import Blueprint
+from flask import current_app
 from flask import url_for
 
 from pagedlist.web import builder as pl_builder
@@ -23,14 +26,45 @@ class Builder(object):
         return lambda page: url_for(endpoint, page=page)
 
 
+def pagedlist_static_for(filename, use_minified=None):
+    """Resource finding function, also available in templates.
+
+    :param filename: File to find a URL for.
+    :param use_minified': If set to ``True``/``False``, use/don't use
+                          minified. If ``None``, use the default setting
+                          from ``PAGEDLIST_USE_MINIFIED``.
+    :return: A URL.
+    """
+    config = current_app.config
+
+    if use_minified is None:
+        use_minified = config['PAGEDLIST_USE_MINIFIED']
+    if use_minified:
+        filename = '.min'.join(os.path.splitext(filename))
+    return url_for('pagedlist.static', filename=filename)
+
+
 class PagedList(object):
     def __init__(self, app=None):
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
+        # Set default configs
+        app.config.setdefault('PAGEDLIST_USE_MINIFIED', True)
         app.config.setdefault('PAGEDLIST_PREFIX', 'pl_')
 
+        # For static files
+        blueprint = Blueprint(
+            'pagedlist',
+            __name__,
+            static_folder='static',
+            static_url_path=app.static_url_path + '/pagedlist')
+        app.register_blueprint(blueprint)
+
+        app.jinja_env.globals['pagedlist_static_for'] = pagedlist_static_for
+
+        # Helpers for jinja2
         prefix = app.config['PAGEDLIST_PREFIX']
         app.jinja_env.globals['%sbuilder' % prefix] = Builder()
         app.jinja_env.globals['%soptions' % prefix] = pl_options
